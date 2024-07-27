7c57ff44e2d42fd514d45eca6aa9aa84a33b86f0
jonatas <capsulebit@gmail.com>
2024-07-26 23:20:06 -0300

    Trabalhando com ImageField no template e nos forms

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -4,8 +4,10 @@ from contact.forms import ContactForm
 from contact.models import Contact
 def create(request):
     form_action = reverse('contact:create')
+
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, request.FILES)
+
         context = {
             'form': form,
             'form_action': form_action,
@@ -32,8 +34,10 @@ def update(request, contact_id):
         Contact, pk=contact_id, show=True
     )
     form_action = reverse('contact:update', args=(contact_id,))
+
     if request.method == 'POST':
-        form = ContactForm(request.POST, instance=contact)
+        form = ContactForm(request.POST, request.FILES, instance=contact)
+
         context = {
             'form': form,
             'form_action': form_action,
@@ -55,18 +59,14 @@ def update(request, contact_id):
         'contact/create.html',
         context
     )
-
-
 def delete(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
     )
     confirmation = request.POST.get('confirmation', 'no')
-
     if confirmation == 'yes':
         contact.delete()
         return redirect('contact:index')
-
     return render(
         request,
         'contact/contact.html',
